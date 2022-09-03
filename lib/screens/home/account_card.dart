7caa282db74144998813b0025bd1bb4620d83b01
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2022-09-03 20:33:41 +0700

    add InkWell

--- lib/screens/home/account_card.dart
+++ lib/screens/home/account_card.dart
@@ -15,111 +15,117 @@ class AccountCard extends StatelessWidget {
     double width = MediaQuery. of(context). size. width ;
     double height = MediaQuery. of(context). size. height;
 
-    return GestureDetector(
-      onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+    return InkWell(
+      hoverColor: Colors.blue[500]!.withOpacity(0.5),
+      onTap: () {},
+      // highlightColor: Colors.blueGrey,
 
-        return RepositoryPage( user: user,);
-      })),
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
 
-      child: Container(
-        height: 230,
-        // color: Colors.deepOrange,
-        margin: EdgeInsets.symmetric(vertical: 5),
-        width: width,
-        // padding: EdgeInsets.all(8),
-        // margin: EdgeInsets.all(8),
-        // decoration: BoxDecoration(
-        //   color: Colors.green[200],
-        //   borderRadius: BorderRadius.circular(16),
-        // ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 20,
-              left: 15,
-              // right: width*0.1,
-              bottom: 15,
-              child: Material(
-                child: Container(
+          return RepositoryPage( user: user,);
+        })),
 
-                  height: 190,
-                  width: width,
-                  decoration: BoxDecoration(
+        child: Container(
+          height: 230,
+          // color: Colors.deepOrange,
+          margin: EdgeInsets.symmetric(vertical: 5),
+          width: width,
+          // padding: EdgeInsets.all(8),
+          // margin: EdgeInsets.all(8),
+          // decoration: BoxDecoration(
+          //   color: Colors.green[200],
+          //   borderRadius: BorderRadius.circular(16),
+          // ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 20,
+                left: 15,
+                // right: width*0.1,
+                bottom: 15,
+                child: Material(
+                  child: Container(
 
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(0.2),
-                    boxShadow: [ BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        offset: const Offset(-10, 10),
-                        blurRadius: 20,
-                        spreadRadius: 4
-                    )],
+                    height: 190,
+                    width: width,
+                    decoration: BoxDecoration(
+
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(0.2),
+                      boxShadow: [ BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          offset: const Offset(-10, 10),
+                          blurRadius: 20,
+                          spreadRadius: 4
+                      )],
+
+                    ),
 
                   ),
 
-                ),
-
-            )),
-            Positioned(
-              top: 0,
-              left: width*0.05,
-              child: Card(
-                elevation: 10,
-                shadowColor: Colors.grey.withOpacity(0.4),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)
-                ),
-                child: Container(
-                  height: 200,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(user.avatarUrl!)
-                    )
+              )),
+              Positioned(
+                top: 0,
+                left: width*0.05,
+                child: Card(
+                  elevation: 10,
+                  shadowColor: Colors.grey.withOpacity(0.4),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
                   ),
-                ),
-              )
-            ),
-            Positioned(
-              top: 45,
-              left: width>600 ? width*1/3 : 200,
-              // right: 60,
-              child: Container(
-                // color: Colors.amberAccent,
-                width: width>500 ? width*0.5 : 150,
-                height: 150,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    AutoSizeText(
-                      user.login!,
-                      maxLines: 1,
-                      style: const TextStyle(
-                        color: Colors.blueAccent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-
-                      ),
+                  child: Container(
+                    height: 200,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(user.avatarUrl!)
+                      )
                     ),
-                    Divider(color: Colors.blueGrey,
-                      height: 40,
-                    ),
-                    AutoSizeText(user.htmlUrl!,
-                      maxLines: 2,
-                      style: TextStyle(
-                          color: Colors.grey,
+                  ),
+                )
+              ),
+              Positioned(
+                top: 45,
+                left: width>600 ? width*1/3 : 200,
+                // right: 60,
+                child: Container(
+                  // color: Colors.amberAccent,
+                  width: width>500 ? width*0.5 : 150,
+                  height: 150,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      AutoSizeText(
+                        user.login!,
+                        maxLines: 1,
+                        style: const TextStyle(
+                          color: Colors.blueAccent,
                           fontWeight: FontWeight.bold,
-                          // fontSize: 16
-                      ),
-                    ),
+                          fontSize: 20,
 
-                  ],
-                ),
-            ))
-          ],
-        )
+                        ),
+                      ),
+                      Divider(color: Colors.blueGrey,
+                        height: 40,
+                      ),
+                      AutoSizeText(user.htmlUrl!,
+                        maxLines: 2,
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            // fontSize: 16
+                        ),
+                      ),
+
+                    ],
+                  ),
+              ))
+            ],
+          )
+        ),
       ),
     );
   }
